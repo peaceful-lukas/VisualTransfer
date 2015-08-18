13cd7636bfb65bae2b2ebba6116d7ea74f5b3fd7
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-18 15:53:34 +0900

    param minor

--- util/getParam.m
+++ util/getParam.m
@@ -149,7 +149,6 @@ function param = getAwaParam(method)
         % param.knn_const = 3; % constant for constructing k-nn graph.
         % param.c_lm = 10; % large margin for classification
         % param.sp_lm = 0.1; % large margin for structure preserving
-        % param.sim_bound = 30; % lower bound for the similarities in the pulling term
         % param.lambda_W = 100000; % regularizer coefficient
         % param.lambda_U = 100; % regularizer coefficient
         % param.alpha = 5; % softmax parameter.
@@ -178,23 +177,43 @@ function param = getAwaParam(method)
         % param.bal_c = 1;
         % param.bal_sp = 10;
 
-        (3)
+        % (3)
+        % param.numClasses = 50;
+        % param.maxIterW = 1000;
+        % param.maxIterU = 1000;
+        % param.maxAlter = 50;
+        % param.batchSize = 10; % mini-batch size
+        % param.lowDim = 400;
+        % param.featureDim = 9216;
+
+        % param.knn_const = 3; % constant for constructing k-nn graph.
+        % param.c_lm = 10; % large margin for classification
+        % param.sp_lm = 0.1; % large margin for structure preserving
+        % param.lambda_W = 1000000; % regularizer coefficient
+        % param.lambda_U = 10000; % regularizer coefficient
+        % param.alpha = 5; % softmax parameter.
+        % param.lr_W = 0.00001; % learning rate for W
+        % param.lr_U = 0.00001; % learning rate for U
+        % param.bal_c = 1;
+        % param.bal_sp = 10;
+
+        (4)
         param.numClasses = 50;
         param.maxIterW = 1000;
         param.maxIterU = 1000;
         param.maxAlter = 50;
         param.batchSize = 10; % mini-batch size
-        param.lowDim = 400;
+        param.lowDim = 100;
         param.featureDim = 9216;
 
         param.knn_const = 3; % constant for constructing k-nn graph.
         param.c_lm = 10; % large margin for classification
         param.sp_lm = 0.1; % large margin for structure preserving
-        param.lambda_W = 1000000; % regularizer coefficient
-        param.lambda_U = 10000; % regularizer coefficient
+        param.lambda_W = 100000; % regularizer coefficient
+        param.lambda_U = 100; % regularizer coefficient
         param.alpha = 5; % softmax parameter.
-        param.lr_W = 0.00001; % learning rate for W
-        param.lr_U = 0.00001; % learning rate for U
+        param.lr_W = 0.000001; % learning rate for W
+        param.lr_U = 0.000001; % learning rate for U
         param.bal_c = 1;
         param.bal_sp = 10;
 
