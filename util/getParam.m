dac4f748ccc40d23d43f7d5b5e8b3c7633a18c9e
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-18 16:43:51 +0900

    minor

--- util/getParam.m
+++ util/getParam.m
@@ -197,7 +197,27 @@ function param = getAwaParam(method)
         % param.bal_c = 1;
         % param.bal_sp = 10;
 
-        (4)
+        % (4)
+        % param.numClasses = 50;
+        % param.maxIterW = 1000;
+        % param.maxIterU = 1000;
+        % param.maxAlter = 50;
+        % param.batchSize = 10; % mini-batch size
+        % param.lowDim = 100;
+        % param.featureDim = 9216;
+
+        % param.knn_const = 3; % constant for constructing k-nn graph.
+        % param.c_lm = 10; % large margin for classification
+        % param.sp_lm = 0.1; % large margin for structure preserving
+        % param.lambda_W = 100000; % regularizer coefficient
+        % param.lambda_U = 100; % regularizer coefficient
+        % param.alpha = 5; % softmax parameter.
+        % param.lr_W = 0.000001; % learning rate for W
+        % param.lr_U = 0.000001; % learning rate for U
+        % param.bal_c = 1;
+        % param.bal_sp = 10;
+
+        (5) % same with the (1)
         param.numClasses = 50;
         param.maxIterW = 1000;
         param.maxIterU = 1000;
@@ -212,11 +232,10 @@ function param = getAwaParam(method)
         param.lambda_W = 100000; % regularizer coefficient
         param.lambda_U = 100; % regularizer coefficient
         param.alpha = 5; % softmax parameter.
-        param.lr_W = 0.000001; % learning rate for W
-        param.lr_U = 0.000001; % learning rate for U
+        param.lr_W = 0.00001; % learning rate for W
+        param.lr_U = 0.00001; % learning rate for U
         param.bal_c = 1;
         param.bal_sp = 10;
-
 
 
     elseif strcmp(method, 'lmspe_le')
