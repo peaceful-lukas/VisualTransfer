a7687144e3fcd1959d628d0e7f5d63c154a7838a
peaceful-lukas <peaceful.lukas@gmail.com>
2015-08-20 05:48:49 +0900

    rollback to scale gradient

--- lme/lmspe/learnU_lmspe.m
+++ lme/lmspe/learnU_lmspe.m
@@ -73,10 +73,10 @@ function dU = computeGradient(DS, W, U, cTriplets, spTriplets, param)
         sp_dU = sp_dU/size(param.spTriplets, 1);
     end
 
-    % if( num_spTriplets > 0 )
-    %     ratio = sqrt(norm(c_dU, 'fro')/norm(sp_dU, 'fro'));
-    %     sp_dU = ratio*sp_dU;
-    % end
+    if( num_spTriplets > 0 )
+        ratio = sqrt(norm(c_dU, 'fro')/norm(sp_dU, 'fro'));
+        sp_dU = ratio*sp_dU;
+    end
     dU = bal_c*c_dU + bal_sp*sp_dU + lambda_U*U/size(U, 2);
 end
 
