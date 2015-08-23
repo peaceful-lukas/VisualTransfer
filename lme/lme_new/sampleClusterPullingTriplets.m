695c92445c908febefe63f4757379d45e68f8f73
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-24 04:33:47 +0900

    batch size missing

--- lme/lme_new/sampleClusterPullingTriplets.m
+++ lme/lme_new/sampleClusterPullingTriplets.m
@@ -3,7 +3,7 @@ function pTriplets = sampleClusterPullingTriplets(DS, W, U, param)
 
 X = DS.D;
 num_pTriplets = size(param.pTriplets, 1);
-pTriplets = param.pTriplets;
+pTriplets = param.pTriplets(randperm(num_pTriplets, param.p_batchSize), :);
 
 loss = param.p_lm + diag((W*X(:, pTriplets(:, 1)))' * (U(:, pTriplets(:, 3)) - U(:, pTriplets(:, 2))));
 valids = find(loss > 0);
