a62076bfa09fcda4f8369e2541f836ee7aaa0935
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-24 05:23:19 +0900

    dispAcc added.

--- util/dispAccuracy.m
+++ util/dispAccuracy.m
@@ -55,5 +55,31 @@ function [train_acc test_acc] = dispAccuracy(method, iter, DS, W, U, param, numP
         classified = ceil(classified/param.numPrototypes);
         test_acc = numel(find(DS.TL == classified))/numel(DS.TL);
         fprintf('Alternation %d) TEST set accuracy :  %.4f\n', iter, test_acc);
+    
+
+    elseif strcmp(method, 'lme_new')
+        cumNumProto = cumsum(param.numPrototypes);
+        [~, classified_raw] = max(DS.D'*W'*U, [], 2);
+        classified = zeros(numel(classified_raw), 1);
+        for c = 1:param.numClasses
+            t = find(classified_raw <= cumNumProto(c));
+            classified(t) = c;
+            classified_raw(t) = Inf;
+        end
+        train_acc = numel(find(DS.DL == classified))/numel(DS.DL);
+        fprintf('Alternation %d) train set accuracy : %.4f\n', iter, train_acc);
+
+
+        cumNumProto = cumsum(param.numPrototypes);
+        [~, classified_raw] = max(DS.T'*W'*U, [], 2);
+        classified = zeros(numel(classified_raw), 1);
+        for c = 1:param.numClasses
+            t = find(classified_raw <= cumNumProto(c));
+            classified(t) = c;
+            classified_raw(t) = Inf;
+        end
+        test_acc = numel(find(DS.TL == classified))/numel(DS.TL);
+        fprintf('Alternation %d) TEST set accuracy :  %.4f\n', iter, test_acc);
     end
+
 end
