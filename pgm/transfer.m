2201b631e95aba15d7d1970beee041dd1cded9b4
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-25 07:05:39 +0900

    fix

--- pgm/transfer.m
+++ pgm/transfer.m
@@ -2,19 +2,23 @@ function [U_new U] = transfer(DS, W, U, c1, c2, target, param)
 
 % target : the prototype in class 'c1'
 
+numProto_c1 = param.numPrototypes(c1);
+numProto_c2 = param.numPrototypes(c2);
+protoStartIdx = [0 cumsum(param.numPrototypes)];
+U_c1 = U(:, protoStartIdx(c1)+1:protoStartIdx(c1+1));
+U_c2 = U(:, protoStartIdx(c2)+1:protoStartIdx(c2+1));
+
+simMatrix = U_c1'*U_c2;
+sim_scores = sort(simMatrix(:), 'descend');
 
 %%%%%% Graph Matching - RRWM
 param_gm.maxIterGM = 10;
-param_gm.match_thrsh = 0.1;
-param_gm.match_sim_thrsh = 0.05;
+param_gm.match_thrsh = sim_scores(min(numProto_c1, numProto_c2));
+param_gm.match_sim_thrsh = sim_scores(max(numProto_c1, numProto_c2));
 param_gm.knn1 = 3;
 param_gm.knn2 = 4;
 param_gm.voting_alpha = 10;
 
-protoStartIdx = [0 cumsum(param.numPrototypes)];
-
-U_c1 = U(:, protoStartIdx(c1)+1:protoStartIdx(c1+1));
-U_c2 = U(:, protoStartIdx(c2)+1:protoStartIdx(c2+1));
 
 [X_sol, cand_matches] = progGM(U_c1, U_c2, param_gm);
 matched_pairs = cand_matches(find(X_sol), :);
