67f3bf0e9567b4af2668ce82b20c9ad727e044dd
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-08-24 22:35:09 +0900

    gradient arrayfun

--- lme/lme_new/learnW_new.m
+++ lme/lme_new/learnW_new.m
@@ -40,14 +40,27 @@ num_pTriplets = size(pTriplets, 1);
 
 c_dW = zeros(size(W));
 if num_cTriplets > 0
-    c_dW = (U(:, cTriplets(:, 3)) - U(:, cTriplets(:, 2))) * X(:, cTriplets(:, 1))';
+    % c_dW = (U(:, cTriplets(:, 3)) - U(:, cTriplets(:, 2))) * X(:, cTriplets(:, 1))';
+    % c_dW = c_dW/param.c_batchSize;
+
+    c_dW_cell = arrayfun(@(n) (U(:, cTriplets(n, 3)) - U(:, cTriplets(n, 2)))*X(:, cTriplets(n, 1))', 1:num_cTriplets, 'UniformOutput', false);
+    c_dW_cat = cat(3, c_dW_cell{:});
+    c_dW = sum(c_dW_cat, 3);
+    
     c_dW = c_dW/param.c_batchSize;
 end
 
 p_dW = zeros(size(W));
 if num_pTriplets > 0
-    p_dW = (U(:, pTriplets(:, 3)) - U(:, pTriplets(:, 2))) * X(:, pTriplets(:, 1))';
+    % p_dW = (U(:, pTriplets(:, 3)) - U(:, pTriplets(:, 2))) * X(:, pTriplets(:, 1))';
+    % p_dW = p_dW/param.p_batchSize;
+
+    p_dW_cell = arrayfun(@(n) (U(:, pTriplets(n, 3)) - U(:, pTriplets(n, 2)))*X(:, pTriplets(n, 1))', 1:num_pTriplets, 'UniformOutput', false);
+    p_dW_cat = cat(3, p_dW_cell{:});
+    p_dW = sum(p_dW_cat, 3);
+
     p_dW = p_dW/param.p_batchSize;
 end
 
 dW = param.bal_c*c_dW + param.bal_p*p_dW + param.lambda_W*W;
+
