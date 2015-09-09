e04de0b86946499a88c9db172520b4d96e58e9b8
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-09-10 02:05:34 +0900

    bug

--- transfer/local_train.m
+++ transfer/local_train.m
@@ -10,33 +10,33 @@ U_retrained = learnU_new(DS, W, U_new, param_new);
 
 function sTriplets_local = localStructurePreservingTriplets(param_new, trainTargetClasses)
 
-    startProtoIdx = [0 cumsum(param_new.numPrototypes)];
-    sTriplets_local = [];
+startProtoIdx = [0 cumsum(param_new.numPrototypes)];
+sTriplets_local = [];
 
-    for i=1:length(trainTargetClasses)
-        cls = trainTargetClasses(i);
-        protoOffset = startProtoIdx(cls);
+for i=1:length(trainTargetClasses)
+    cls = trainTargetClasses(i);
+    protoOffset = startProtoIdx(cls);
 
-        A = param_new.knnGraphs{cls};
+    A = param_new.knnGraphs{cls};
+    
+    for j=1:length(size(A, 2))
+        neighbors = find(A(:, j) == 1);
+        non_neighbors = find(A(:, j) == 0);
+        non_neighbors(find(non_neighbors == j)) = [];
+
+        neighbors = neighbors + protoOffset;
+        non_neighbors = non_neighbors + protoOffset;
         
-        for j=1:length(size(A, 2))
-            neighbors = find(A(:, j) == 1);
-            non_neighbors = find(A(:, j) == 0);
-            non_neighbors(find(non_neighbors == j)) = [];
+        sTriplets_local_j = zeros(numel(neighbors) * numel(non_neighbors), 3);
 
-            neighbors = neighbors + protoOffset;
-            non_neighbors = non_neighbors + protoOffset;
-            
-            sTriplets_local_j = zeros(numel(neighbors) * numel(non_neighbors), 3);
+        tmp_sec_col = repmat(neighbors, numel(non_neighbors), 1);
 
-            tmp_sec_col = repmat(neighbors, numel(non_neighbors), 1);
+        sTriplets_local_j(:, 1) = repmat(protoOffset+j, numel(neighbors) * numel(non_neighbors), 1);
+        sTriplets_local_j(:, 2) = tmp_sec_col(:);
+        sTriplets_local_j(:, 3) = repmat(non_neighbors, numel(neighbors), 1);
 
-            sTriplets_local_j(:, 1) = repmat(protoOffset+j, numel(neighbors) * numel(non_neighbors), 1);
-            sTriplets_local_j(:, 2) = tmp_sec_col(:);
-            sTriplets_local_j(:, 3) = repmat(non_neighbors, numel(neighbors), 1);
-
-            sTriplets_local = [sTriplets_local; sTriplets_local_j];
-        end
+        sTriplets_local = [sTriplets_local; sTriplets_local_j];
     end
 end
+
 
