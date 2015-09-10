c91e5c6058c71b98d77fff60149740eeb8638373
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-09-10 21:53:21 +0900

    local structure preserving triplets

--- transfer/local_train.m
+++ transfer/local_train.m
@@ -11,16 +11,47 @@ fprintf('after\n');
 
 
 
-function sTriplets_local = localStructurePreservingTriplets(param_new, trainTargetClasses)
+function sTriplets = localStructurePreservingTriplets(param_new, trainTargetClasses)
+
+% startProtoIdx = [0 cumsum(param_new.numPrototypes)];
+% sTriplets_local = [];
+
+% for i=1:length(trainTargetClasses)
+%     cls = trainTargetClasses(i);
+%     protoOffset = startProtoIdx(cls);
+
+%     A = param_new.knnGraphs{cls};
+    
+%     for j=1:length(size(A, 2))
+%         neighbors = find(A(:, j) == 1);
+%         non_neighbors = find(A(:, j) == 0);
+%         non_neighbors(find(non_neighbors == j)) = [];
+
+%         neighbors = neighbors + protoOffset;
+%         non_neighbors = non_neighbors + protoOffset;
+        
+%         num_sTriplets_local_j = numel(neighbors) * numel(non_neighbors);
+        
+%         if num_sTriplets_local_j > 0
+%             sTriplets_local_j = zeros(num_sTriplets_local_j, 3);
+
+%             sTriplets_local_j(:, 1) = repmat(protoOffset+j, num_sTriplets_local_j, 1);
+%             sTriplets_local_j(:, 2) = repmat(neighbors, numel(non_neighbors), 1);
+%             sTriplets_local_j(:, 3) = repmat(non_neighbors, numel(neighbors), 1);
+
+%             sTriplets_local = [sTriplets_local; sTriplets_local_j];
+%         end
+%     end
+% end
+
 
 startProtoIdx = [0 cumsum(param_new.numPrototypes)];
-sTriplets_local = [];
+sTriplets = [];
 
-for i=1:length(trainTargetClasses)
-    cls = trainTargetClasses(i);
-    protoOffset = startProtoIdx(cls);
-
-    A = param_new.knnGraphs{cls};
+for i=1:length(param_new.numClasses)
+    
+    protoOffset = startProtoIdx(i);
+    A = param_new.knnGraphs{i};
     
     for j=1:length(size(A, 2))
         neighbors = find(A(:, j) == 1);
@@ -30,16 +61,16 @@ for i=1:length(trainTargetClasses)
         neighbors = neighbors + protoOffset;
         non_neighbors = non_neighbors + protoOffset;
         
-        num_sTriplets_local_j = numel(neighbors) * numel(non_neighbors);
+        num_sTriplets_j = numel(neighbors) * numel(non_neighbors);
         
-        if num_sTriplets_local_j > 0
-            sTriplets_local_j = zeros(num_sTriplets_local_j, 3);
+        if num_sTriplets_j > 0
+            sTriplets_j = zeros(num_sTriplets_j, 3);
 
-            sTriplets_local_j(:, 1) = repmat(protoOffset+j, num_sTriplets_local_j, 1);
-            sTriplets_local_j(:, 2) = repmat(neighbors, numel(non_neighbors), 1);
-            sTriplets_local_j(:, 3) = repmat(non_neighbors, numel(neighbors), 1);
+            sTriplets_j(:, 1) = repmat(protoOffset+j, num_sTriplets_j, 1);
+            sTriplets_j(:, 2) = repmat(neighbors, numel(non_neighbors), 1);
+            sTriplets_j(:, 3) = repmat(non_neighbors, numel(neighbors), 1);
 
-            sTriplets_local = [sTriplets_local; sTriplets_local_j];
+            sTriplets = [sTriplets; sTriplets_j];
         end
     end
 end
