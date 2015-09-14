3fbc83f8ee292eedf882026269b8bab370282d8e
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-09-14 10:46:22 +0900

    maximum scores between prototypes of two classes

--- transfer/transfer_demo.m
+++ transfer/transfer_demo.m
@@ -1,36 +1,5 @@
 
-% L = {};
-% for i=1:param.numClasses
-%     L{i} = laplacian(param.knnGraphs{i}, 1); % normalized
-% end
-
-% M = zeros(param.numClasses, param.numClasses); % prematched score matrix by graph laplacians
-% for i=1:param.numClasses
-%     for j=1:param.numClasses
-%         M(i, j) = prematching(L{i}, L{j});
-%     end
-% end
-
-
-
-protoStartIdx = [0 cumsum(param.numPrototypes)];
-S = zeros(param.numClasses, param.numClasses); % Similarity between class prototype distributions
-
-for i=1:param.numClasses
-    for j=1:param.numClasses
-        if i == j
-            S(i, j) = -Inf;
-        else
-            S(i, j) = mean(U(:, protoStartIdx(i)+1:protoStartIdx(i+1)), 2)'*mean(U(:, protoStartIdx(j)+1:protoStartIdx(j+1)), 2);
-        end
-    end
-end
-
-[maxS, maxS_idx] = max(S, [], 1)
-
-transferPairs = [maxS_idx' (1:12)']; % ------> (transfer direction)
-
-
+[tPairs S] = transferPairs(U, param);
 
 param0 = param;
 param_new = param;
@@ -38,13 +7,11 @@ param_new = param;
 U0 = U;
 U_new = U;
 
-for i=1:size(transferPairs, 1)
-    fprintf('\n\n\n\n==========================================================================\n');
-    fprintf('================================ class %d ================================\n', i);
-    fprintf('==========================================================================\n');
+for i=1:size(tPairs, 1)
+    fprintf('\n\n\n================================ class %d ================================\n', i);
 
-    c1 = transferPairs(i, 1);
-    c2 = transferPairs(i, 2);
+    c1 = tPairs(i, 1);
+    c2 = tPairs(i, 2);
     scale_alpha = 1.0;
 
     [U_new, param_new, ~, matched_pairs, trainTargetClasses] = transfer(DS, W, U_new, U0, c1, c2, scale_alpha, param_new);
@@ -57,7 +24,6 @@ for i=1:size(transferPairs, 1)
     % [U_retrained param_new] = local_train(DS, W, U_new, param_new, trainTargetClasses);
 
     transfer_dispAccuracies(DS, W, U, U_new, param_new.numPrototypes, param0);
-
 end
 
 
