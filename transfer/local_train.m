81adc2214378fe7dabb2bace961277dceac8abaf
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-09-16 19:34:07 +0900

    minor

--- transfer/local_train.m
+++ transfer/local_train.m
@@ -6,7 +6,7 @@ param_new.cTriplets = generateClassificationTriplets(DS, param_new);
 param_new.sTriplets = generateLocasStructurePreservingTriplets(param_new, trainTargetClasses);
 
 % locally learn prototypes (U)
-W_retrained = local_learnW(DS, W, U_new, param_new, trainTargetClasses);
+% W_retrained = local_learnW(DS, W, U_new, param_new, trainTargetClasses);
 U_retrained = local_learnU(DS, W, U_new, param_new, trainTargetClasses);
 
 fprintf('Local LME RESULT\n');
