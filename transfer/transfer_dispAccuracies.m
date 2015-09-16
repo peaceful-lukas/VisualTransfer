b2b48990569cc0a1f53b4e60844ba84ea23e903b
Taewoo Lukas Kim <peaceful.lukas@gmail.com>
2015-09-16 20:33:42 +0900

    minor

--- transfer/transfer_dispAccuracies.m
+++ transfer/transfer_dispAccuracies.m
@@ -1,8 +1,8 @@
 function acc_list = transfer_dispAccuracies(DS, W_new, U_new, W0, U0, param_new, param0);
 acc_list = [];
-for cls = 1:param.numClasses
-    orig_acc = getOriginalAccuracy(cls, DS, W0, U0, param);
-    new_acc = getNewAccuracy(cls, DS, W_new, U_new, param_new, param);
+for cls = 1:param_new.numClasses
+    orig_acc = getOriginalAccuracy(cls, DS, W0, U0, param0);
+    new_acc = getNewAccuracy(cls, DS, W_new, U_new, param_new);
     acc_list = [acc_list; orig_acc new_acc];
     fprintf('Accuracy (class %d) : %.4f ----> %.4f\n', cls, orig_acc, new_acc);    
 end
@@ -11,15 +11,15 @@ end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function orig_acc = getOriginalAccuracy(cls, DS, W0, U0, param)
+function orig_acc = getOriginalAccuracy(cls, DS, W0, U0, param0)
 
 % %%%%%% Disp Accuracy
-cumNumProto = cumsum(param.numPrototypes);
+cumNumProto = cumsum(param0.numPrototypes);
 classIdx = find(DS.TL == cls);
 class_feat = DS.T(:, classIdx);
 [~, classified_raw]= max(class_feat'*W0'*U0, [], 2);
 classified = zeros(numel(classified_raw), 1);
-for c = 1:param.numClasses
+for c = 1:param0.numClasses
     t = find(classified_raw <= cumNumProto(c));
     classified(t) = c;
     classified_raw(t) = Inf;
@@ -30,14 +30,14 @@ orig_acc = numel(find(classified == cls))/numel(find(DS.TL == cls));
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function new_acc = getNewAccuracy(cls, DS, W_new, U_new, param_new, param)
+function new_acc = getNewAccuracy(cls, DS, W_new, U_new, param_new)
 
 cumNumProto = cumsum(param_new.numPrototypes);
 classIdx = find(DS.TL == cls);
 class_feat = DS.T(:, classIdx);
 [~, classified_raw]= max(class_feat'*W_new'*U_new, [], 2);
 classified = zeros(numel(classified_raw), 1);
-for c = 1:param.numClasses
+for c = 1:param_new.numClasses
     t = find(classified_raw <= cumNumProto(c));
     classified(t) = c;
     classified_raw(t) = Inf;
